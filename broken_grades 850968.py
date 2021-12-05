ea47edb70d5dda900c8a52c10fc2532440a9664b
Kashish1311 <KK1269@live.mdx.ac.uk>
2021-12-05 12:00:01 +0400

    corrected files

--- broken_grades 850968.py
+++ broken_grades 850968.py
@@ -1,56 +1,34 @@
-# Calculating Grades (ok, let me think about this one)
-
-# Write a program that will average 3 numeric exam grades, return an average test score, a corresponding letter grade, and a message stating whether the student is passing.
-
-# Average	Grade
-# 90+	A
-# 80-89	B
-# 70-79	C
-# 60-69	D
-# 0-59	F
-
-# Exams: 89, 90, 90
-# Average: 90
-# Grade: A
-# Student is passing.
-
-# Exams: 50, 51, 0
-# Average: 33
-# Grade: F
-# Student iis failing.
+#broken grades corrected code
 
 exam_one = int(input("Input exam grade one: "))
+exam_two = int(input("Input exam grade two: "))
+exam_three = int(input("Input exam grade three: "))
 
-exam_two = input("Input exam grade two: "))
+grades = [exam_one, exam_two, exam_three]
+total = 0
+for i in grades:
+   total = (exam_one + exam_two + exam_three)
 
-exam_3 = str(input("Input exam grade three: "))
+avg = total / len(grades)
 
-grades = [exam_one exam_two exam_three]
-sum = 0
-for grade in grade:
-  sum = sum + grade
 
-avg = sum / len(grdes)
-
-if avg >= 90:
-    letter_grade = "A"
-elif avg >= 80 and avg < 90
-    letter_grade = "B"
-elif avg > 69 and avg < 80:
-    letter_grade = "C'
-elif avg <= 69 and avg >= 65:
-    letter_grade = "D"
-elif:
-    letter_grade = "F"
-
-for grade in grades:
-    print("Exam: " + str(grade))
-
-    print("Average: " + str(avg))
-
-    print("Grade: " + letter_grade)
-
-if letter-grade is "F":
-    print "Student is failing."
+if avg > 89:
+   letter_grade = "A"
+elif avg >= 80 and avg < 90:
+   letter_grade = "B"
+elif avg >= 70 and avg < 80:
+   letter_grade = "C"
+elif avg >= 60 and avg < 70:
+   letter_grade = "D"
 else:
-    print "Student is passing."
+   letter_grade = "F"
+
+
+print("Exam: ", (grades))
+print("Average: ", round(float((avg))))
+print("Grade: ", str((letter_grade)))
+
+if letter_grade == "F":
+   print("Student is failing.")
+else:
+   print("Student is passing.")
--- broken_pirate 850968.py
+++ broken_pirate 850968.py
@@ -1,5 +1,9 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+#pirate code corrected 
+
+greeting = input("Hello, possible pirate! What's the password?")
+
+if greeting == ("Arrr!"):
+        
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else:
+        print("Greetings, hater of pirates!")
